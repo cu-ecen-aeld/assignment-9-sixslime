4f968f0288f8ee472ce8f077c0ea40721383a3e7
SixSlime <ryan.mm.taylor@gmail.com>
2026-08-05 16:35:14 -0700

    update git hash

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = 177bcc00f59d998f8ac71d48bd9e7c4d39820d54
+AESDCHAR_VERSION = 00e73a042ee4e3c74c36448afb3ddb13e681343d
 AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sixslime.git
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
